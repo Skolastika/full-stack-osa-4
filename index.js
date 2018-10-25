e66c9479dbf56ef2984b92124881daf5d7ef98f4
Henna-Riikka Ruonala <hennariikka.ruonala@gmail.com>
2018-10-25 11:01:14 +0300

    Finished moduling for 4.2

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-//const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
-const Blog = require('./models/blog')
 
-/*const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog*/
-
-app.use(cors())
-app.use(bodyParser.json())
-
-/*if ( process.env.NODE_ENV !== 'production' ) {
+if ( process.env.NODE_ENV !== 'production' ) {
   require('dotenv').config()
 }
 
 const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)*/
+mongoose.connect(mongoUrl)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
